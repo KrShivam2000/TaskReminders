7121139fabb95d7965249fe141dbf8cd6f942a64
Shivamkr522 <work.shivam522@gmail.com>
2024-08-07 18:59:29 +0530

    resolved mysql and its connectivity issue

--- src/main/java/com/reminder/task/config/TaskApplication.java
+++ src/main/java/com/reminder/task/config/TaskApplication.java
@@ -1,7 +1,9 @@
-package com.reminder.task;
+package com.reminder.task.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskApplication {
